8ae5a168bd145f99b100234d12a6b2cdd4db9b22
core <421076879@qq.com>
2020-03-27 12:33:11 +0800

    测试

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\User \User;
 
 
 use Illuminate\Notifications\Notifiable;
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Models\User::class, function (Faker $faker) {
+$factory->define(App\User \User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
